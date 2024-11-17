eee568c1a456fe4df8b12fef10af90241ae49bda
Harshita <harshitabhadimane02051998@gmail.com>
2024-11-17 14:31:24 +0530

    changed ip addresses

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,2 @@
 component=$1
- ansible-playbook -i 172.31.31.178, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
+ ansible-playbook -i 172.31.29.42, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
